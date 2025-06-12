17b0a02e2a7c6609f164d742d9a805c0f7cbb88e
ATN04 <etienne.kita77@gmail.com>
2025-06-12 18:26:23 +0200

    [EKT] Fix output.c

--- genetic/output.c
+++ genetic/output.c
@@ -4,11 +4,11 @@
 void write_output_to_file(TabScore *ts, double **dist_matrix, double **time_matrix, int N, const char *path) {
     FILE *file = fopen(path, "w");
     int i;
-    fputc('0', file);
-    fputc(' ', file);
+    int started = 0;
     for (i = 0; i < N + NB_TRUCKS_MAX; i++) {
         if (ts->tab[i] > 0) {
-            if(ts->tab[i - 1] <= 0) {
+            if((ts->tab[i - 1] <= 0 || (i == 0 && ts->tab[i - 1] > 0)) && !started) {
+                started = 1;
                 fputc('0', file);
                 fputc(' ', file);
             }
@@ -17,6 +17,7 @@ void write_output_to_file(TabScore *ts, double **dist_matrix, double **time_matr
                 fputc(' ', file);
                 fputc('0', file);
                 fputc('\n', file);
+                started = 0;
             } else {
                 fputc(' ', file);
             }
